3e30405b09de946d7d7ddc1a71c8be11ff762baf
johng <johng@10.17.143.154>
2019-11-25 11:23:33 -0500

    pagerank class

--- CPS_842_Project/src/PageRank.java
+++ CPS_842_Project/src/PageRank.java
@@ -1,6 +1,11 @@
+import java.util.HashMap;
 
 public class PageRank {
 
+	static HashMap<Integer, String> pagerank;
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
