03b839234919aa377c5e04df333ed3e747a8aadf
johng <johng@10.17.143.154>
2019-11-25 13:48:00 -0500

    pagerank

--- CPS_842_Project/src/PageRank.java
+++ CPS_842_Project/src/PageRank.java
@@ -2,10 +2,21 @@ import java.util.HashMap;
 
 public class PageRank {
 
-	static HashMap<Integer, String> citations;
+	private HashMap<Integer, String> citations;
+	private HashMap<Integer, Double> pageRanks;
 	
+	// This constructor builds the page rank file from Invert
 	public PageRank(HashMap<Integer, String> citations) {
 		this.citations = citations;
+	}
+	
+	// This constructor is when getting page rank scores for a document during search
+	public PageRank() {
+		// pageRanks = read page ranks file
+	}
+	
+	public double getPageRank(int docNum) {
+		return pageRanks.get(docNum);
 	}
 	
 	public static void main(String[] args) {
